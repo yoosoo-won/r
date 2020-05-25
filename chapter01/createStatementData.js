1ca430441ebb9d7ae720e14cd7e2599c6826aad0
Yoosoo <shelling104@gmail.com>
2020-05-25 18:14:21 +0900

    PerformanceCalculator클래스 추가

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -1,8 +1,39 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+  get amount() {
+    let result = 0;
+    switch (this.performance.play.type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error (`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
+}
+
 export default function createStatementData (invoice, plays) {
   console.log('createStatementData');
-  const totalAmount = data => data.performances.reduce((total, p) => total + p.amount, 0);
-  const totalVolumeCredits = data => data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  const playFor = aPerformance => plays[aPerformance.playID];
+  const totalAmount = data =>
+      data.performances.reduce((total, p) => total + p.amount, 0);
+  const totalVolumeCredits = data =>
+      data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  const playFor = aPerformance =>
+      plays[aPerformance.playID];
   const enrichPerformance = aPerformance => {
     /**
      * @param aPerformance
@@ -10,36 +41,16 @@ export default function createStatementData (invoice, plays) {
      * @description aPerformance 역할이 명확하지 않는 경우 부정 관사(a/an)을 붙인다고 한다.by 마틴 파울러
      * 항상 명확성을 높이기 위한 이름을 작성하기 위해 노력할 것!!
      */
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = { ...aPerformance };
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     console.log('result', result);
     return result;
   };
-  const amountFor = aPerformance => {
-
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error (`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  };
+  const amountFor = aPerformance =>
+      new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   const volumeCreditsFor = aPerformance => {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
