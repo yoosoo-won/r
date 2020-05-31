4c5a0642e644f527bb71057b01e79be7d6dadebc
Yoosoo <shelling104@gmail.com>
2020-06-01 06:47:10 +0900

    공연료 계산기를 통하여 계산기 서브 클래스 만들기

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -4,34 +4,52 @@ class PerformanceCalculator {
     this.play = aPlay;
   }
   get amount() {
-    let result = 0;
     switch (this.play.type) {
       case 'tragedy': // 비극
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw `오류 발생`; // 비극 공연료는 TragedyCalculator 사용을 유도.
       case 'comedy': // 희극
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
+        throw `서브클래스에서 처리하세요.`;
       default:
         throw new Error (`알 수 없는 장르: ${this.play.type}`);
     }
-    return result;
   }
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ('comedy' === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
     return result;
   }
 }
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+  get volumeCredits() {
+    return Math.floor(this.performance.audience / 5);
+  }
+}
+
+const createPerformanceCalculator = (aPerformance, aPlay) => {
+  switch (aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+};
 
 export default function createStatementData (invoice, plays) {
   console.log('createStatementData');
@@ -48,7 +66,7 @@ export default function createStatementData (invoice, plays) {
      * @description aPerformance 역할이 명확하지 않는 경우 부정 관사(a/an)을 붙인다고 한다.by 마틴 파울러
      * 항상 명확성을 높이기 위한 이름을 작성하기 위해 노력할 것!!
      */
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = { ...aPerformance };
     result.play = calculator.play;
     result.amount = calculator.amount;
